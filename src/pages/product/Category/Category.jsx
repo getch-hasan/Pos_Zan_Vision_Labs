d59650189691d0e87dd510691fc5a2e6fde1562f
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2025-01-11 15:28:58 +0600

    feat:create product category

--- src/pages/product/Category/Category.jsx
+++ src/pages/product/Category/Category.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddButton from '../../../components/button/AddButton';
 import ImportButton from '../../../components/button/ImportButton';
 import CategoryButtons from './CategoryButtons';
 import CategoryTable from './CategoryTable';
 
 const Category = () => {
+  const [clickData, setClickData] = useState([])
+
+  const handleButtonClick = (btn) => {
+    if (!clickData.includes(btn)) {
+      setClickData([...clickData, btn]); // New value add kora hocche
+    } else {
+      setClickData(clickData.filter(item => item !== btn));
+    }
+  };
+  console.log("clickData", clickData);
   return (
     <>
       <div className='md:px-1'>
@@ -12,9 +22,13 @@ const Category = () => {
           <AddButton buttonName="Add Sale" ></AddButton>
           <ImportButton buttonName="Import Category"></ImportButton>
         </div>
-        <div className='mt-5'>
-          <CategoryButtons></CategoryButtons>
-          <CategoryTable></CategoryTable>
+        <div >
+          <div className='mt-5'>
+            <CategoryButtons clickData={clickData} setClickData={setClickData} handleButtonClick={handleButtonClick}></CategoryButtons>
+          </div>
+         <div className='mt-5'>
+         <CategoryTable clickData={clickData} ></CategoryTable>
+         </div>
         </div>
       </div>
     </>
